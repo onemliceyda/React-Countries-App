11053139dd948e0b224ed4e1daa642e25e78c372
Ceyda <ceydaonemli2@gmail.com>
2022-01-28 23:34:23 +0300

    tables fixed,map function updated.

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,92 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table'
+import ReactLoading from 'react-loading';
+
 export default function App() {
 
     const [countries, setCountries] = useState([]);
     const [search, setSearch] = useState('');
     const [filteredCountries, setFilteredCountries] = useState([]);
+    const [loaded, setLoaded] = useState(true);
+
     useEffect(() => {
         axios
             .get('https://restcountries.com/v2/all ')
-            .then(response => setCountries(response.data))
-            .catch(error => console.log({ error })) //olası hatayı yakalamak için kullanılır
+            .then(response => {
+                if (response.status == 200) { //eğer başarılı dönerse (status 200)
+                    setCountries(response.data)
+                    setFilteredCountries(response.data)
+                    setLoaded(false);
+                }
+            })
+            .catch(error => console.log({error})) //olası hatayı yakalamak için kullanılır
     }, [])
 
-    useEffect(() => {
-        const tempArray = []
-        countries.map(country => {
-             if(country.capital.toLowerCase().includes(search.toLowerCase()))
-            {
-                 tempArray.push(country)
-             }
-        })
-        setFilteredCountries(tempArray) 
-    }, [search]) 
-
-
+    const filteredCapitalizeSetter = (searchTerm) => {
+        //bazı ülkelerin capital verisi yok ondan => capital?
+        setFilteredCountries(countries.filter(country => country.capital?.toLocaleLowerCase('tr').includes(searchTerm.toLocaleLowerCase('tr'))))
+    }
 
     return (
         <div className="App">
-            <h1>React Countries App</h1>
-            <input type="text" className="form-control" placeholder="Search" onChange={e => setSearch(e.target.value)} />
-
-
-            {filteredCountries.map(country => {
-                return <div key={country.numericCode}>
-
-                    <Table striped bordered hover size="sm">
-                        <thead>
-                            <tr>
-                                <th>Country</th>
-                                <th>Capital</th>
-                                <th>Region</th>
-                                <th>Flag</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{country.name}</td>
-                                <td>{country.capital}</td>
-                                <td>{country.region}</td>
-                                <td><img src={country.flag} alt={country.name}
-                                    style={{ width: "70px" }} className='rounded mx-auto d-block' /></td>
-                            </tr>
-                        </tbody>
-                    </Table>
+            <h1 className="text-center my-4">
+                <u> React <span className="text-primary">Countries</span> <span className="text-danger">App</span></u>
+            </h1>
+            <div className="container my-2">
+                <div className="row my-4">
+                    <div className="col">
+                        {/* burası capitale göre arar */}
+                        <input type="text" className="form-control shadow border-2 py-3"
+                               placeholder="Capital Search...  "
+                               onChange={e => filteredCapitalizeSetter(e.target.value)}/>
+                    </div>
+                    <div className="col">
+                        {/* burası generale göre arar */}
+                        <input type="text" className="form-control shadow border-2 py-3" placeholder="General Search..."
+                               onChange={e => {
+                                   console.log('General search')
+                               }}/>
+                    </div>
                 </div>
-            })}
+
+                {loaded ?
+                    (
+                        <div className="d-flex justify-content-center align-items-center mt-5">
+                            <ReactLoading type="spin" color="#BC409D" height={400} width={140}/>
+                        </div>
+                    )
+                    :
+                    (
+                        // tablo yapısı
+                        <Table striped responsive="sm" bordered hover className="text-center">
+                            <thead>
+                            <tr>
+                                <th className="w-25">Country</th>
+                                <th className="w-25">Capital</th>
+                                <th className="w-25">Region</th>
+                                <th className="w-25">Flag</th>
+                            </tr>
+                            </thead>
+                            <tbody className="shadow shadow-lg">
+                            {filteredCountries.map(country => (
+                                <tr key={country.numericCode}>
+                                    <td>{country.name}</td>
+                                    <td>{country.capital}</td>
+                                    <td>{country.region}</td>
+                                    <td><img src={country.flag} alt={country.name}
+                                             style={{width: "60px", height: "40px"}}
+                                             className='rounded mx-auto d-block'/>
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
+                    )}
+
+
+            </div>
+
 
         </div>
     )
