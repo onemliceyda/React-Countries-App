b62a707a937c70182f2a0eedcd81cfb40d4f421c
Ceyda <ceydaonemli2@gmail.com>
2022-01-30 12:40:50 +0300

    general search method update

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,7 @@ export default function App() {
     const [search, setSearch] = useState('');
     const [filteredCountries, setFilteredCountries] = useState([]);
     const [loaded, setLoaded] = useState(true);
-    const [searchGeneral,setSearchGeneral] = useState([]);
-    const [searchTitle, setSearchTitle] = useState("");
+
     useEffect(() => {
         axios
             .get('https://restcountries.com/v2/all ')
@@ -20,29 +19,41 @@ export default function App() {
                     setCountries(response.data)
                     setFilteredCountries(response.data)
                     setLoaded(false);
-                    setSearchGeneral(response.data)   
+
 
                 }
             })
             .catch(error => console.log({ error })) //olası hatayı yakalamak için kullanılır
-           
-        }, [])
+
+    }, [])
 
     const filteredCapitalizeSetter = (searchTerm) => {
         //bazı ülkelerin capital verisi yok ondan => capital?
         setFilteredCountries(countries.filter(country => country.capital?.toLocaleLowerCase('tr').includes(searchTerm.toLocaleLowerCase('tr'))))
     }
-    
-    const arama=searchGeneral.filter((value)=>{
-        if (searchTitle === "") {
-            return value;
-          } else if (
-            value.title.toLowerCase().includes(searchTitle.toLowerCase())
-          ) {
-            return value;
-          }
-        }).map((item) => <h5 key={item.id}>{item.title}</h5>)
-    
+
+
+    const excludeColumns = ["country", "capital", "region"];
+
+    const handleChange = value => {
+        setSearch(value);
+        filterData(value);
+    };
+
+
+    const filterData = (value) => {
+        const lowercasedValue = value.toLowerCase().trim();
+        if (lowercasedValue === "") setCountries(countries);
+        else {
+            const filteredData = countries.filter(item => {
+                return Object.keys(item).some(key =>
+                    excludeColumns.includes(key) ? false : item[key].toString().toLowerCase().includes(lowercasedValue)
+                );
+            });
+            setFilteredCountries(filteredData);
+        }
+    }
+
     return (
 
         <div className="App">
@@ -61,7 +72,7 @@ export default function App() {
                     <div className="col">
                         {/* burası generale göre arar */}
                         <input type="text" className="form-control shadow border-2 py-3" placeholder="General Search..."
-                            onChange={e =>arama(e.target.value)}/>
+                            onChange={e => handleChange(e.target.value)/*e =>arama(e.target.value)*/} />
                     </div>
                 </div>
 
